786434be661edfcd9255a2317d0d41fd575dc661
Jag Barpagga <jag@welcomenetworks.com>
2019-10-15 21:39:07 -0700

    designed users.bhs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 //This is starting point for our app.  Initially it was app.js.
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
 require('./db/mongoose')
 const User = require('./models/user')
+
 
 const app = express()
 const port = process.env.PORT || 3000
 
+//setting up directory path
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.use(express.static(publicDirPath));
 //parse incoming json request
 app.use(express.json())
+
+//setting templating engine
+app.set('view engine', 'hbs');
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('/usermanager', (req, res) => {
+    res.render('usermanager', {
+        title: "User Manager",
+        name: "Jag Barpagga"
+    })
+})
+
+app.get('/users', (req, res) => {
+    res.render('users', {
+        title: "Add User...",
+        name: "Jag Barpagga"
+    })
+})
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Login Page",
+        name: "Jag Barpagga"
+    })
+})
+
+
+
 
 app.post('/users', (req, res) => {
     const user = new User(req.body);
@@ -28,5 +66,7 @@ app.post('/users', (req, res) => {
 
 
 app.listen(port, () => {
+    console.log(__dirname)
     console.log("Server is running on port " + port)
+    
 })
